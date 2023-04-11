bff3502597ab57bd2ac2e3a90c65c2ff77a6ecf5
Navpreet Virk <nvirkcom@gmail.com>
2023-04-11 11:07:23 +0530

    Created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<ForgotPassword />} path="/forgot-password" />
+        <Route element={<Home />} path="/" />
+        <Route element={<Offers />} path="/offers" />
+        <Route element={<Profile />} path="/profile" />
+        <Route element={<SignIn />} path="/sign-in" />
+        <Route element={<SignUp />} path="/sign-up" />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
